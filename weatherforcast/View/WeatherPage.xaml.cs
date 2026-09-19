79246d79238c747a6f4b59b2d1e5116c69d02d8e
peter <peterhombolygo@tuta.com>
2026-09-19 13:56:13 -0700

    thingy

--- weatherforcast/View/WeatherPage.xaml.cs
+++ weatherforcast/View/WeatherPage.xaml.cs
@@ -1,9 +1,27 @@
-﻿namespace WeatherApp.View;
+﻿using WeatherApp.ViewModel;
+
+namespace WeatherApp.View;
 
 public partial class WeatherPage : ContentPage
 {
+    public List<Model.List> WeatherList { get; set; } = new List<Model.List>();
+    public double latitude;
+    public double longitude;
+
+    
+
     public WeatherPage()
     {
         InitializeComponent();
+    }
+    
+    private void TapLocation_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapSearch_Tapped(object sender, TappedEventArgs e)
+    {
+
     }
 }
